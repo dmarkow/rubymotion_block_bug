935c9c7113c58c0f2a476df631abe646cf5d333a
Dylan Markow <dylan@dylanmarkow.com>
2012-09-24 07:59:33 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+
+gem 'bubble-wrap', path: '/Users/dylan/dev/rm-bubblewrap'
+
---
+++ app/app_delegate.rb
@@ -0,0 +1,51 @@
+module HTTP
+  class Query
+    attr_accessor :url, :callback
+
+    def initialize(url_string, callback)
+      @callback = callback
+      request = NSMutableURLRequest.requestWithURL(NSURL.URLWithString(url_string), cachePolicy: NSURLRequestUseProtocolCachePolicy, timeoutInterval: 60.0)
+      connection = NSURLConnection.connectionWithRequest(request, delegate: self)
+    end
+
+    def connectionDidFinishLoading(connection)
+      @callback.call('foo')
+    end
+  end
+
+  def self.get(url, &block)
+    ::HTTP::Query.new(url, block)
+  end
+end
+
+class Foo
+  def bar
+    NSLog "Getting request"
+    HTTP.get("http://www.google.com") do |response|
+      NSLog "Got Response"
+    end
+  end
+end
+
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    # Using this does not work; it seems that the response block in Foo#bar
+    # isn't being retained properly, so by the time connectionDidFinishLoading
+    # calls it, it no longer exists and crashes.
+    #
+    Foo.new.bar
+
+    # Calling HTTP#get directly instead of using a separate class works fine.
+    #
+    # HTTP.get("http://www.google.com") do |response|
+    #   NSLog "Got a response..."
+    # end
+
+    # Using this works, however:
+    #
+    # @foo = Foo.new
+    # @foo.bar
+
+    true
+  end
+end
